c28efa36734455939a7dd5ca4575ada343c01278
dankisdead <dong.jin25@ycdsbk12.ca>
2024-01-24 09:16:27 -0500

    finished code

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(973, 214);
   }
 
   /** 
@@ -16,21 +16,32 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  public void draw() {  
+  // stem
+    stroke(53,94,59);
+    strokeWeight((float)(width * 0.03));
+    line((float)(width/2), (float)(height/2), (float)(width/2), (float)(height*0.9));
+    
+  // change strokeweight back to normal
+    strokeWeight((float)(width * 0.005));
+    stroke(0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // petals
+    fill(255,254,224);
+    ellipse((float)(width * 0.65), (float)(height * 0.3), (float)(width * 0.3), (float)(height * 0.3));
+    ellipse((float)(width * 0.35), (float)(height * 0.3), (float)(width * 0.3), (float)(height * 0.3));
+    ellipse((float)(width * 0.65), (float)(height * 0.6), (float)(width * 0.3), (float)(height * 0.3));
+    ellipse((float)(width * 0.35), (float)(height * 0.6), (float)(width * 0.3), (float)(height * 0.3));
+
+  // middle of flower
+    fill(254,229,86);
+    ellipse((float)(width * 0.5),(float)(height * 0.45),(float)(width * 0.3), (float)(height * 0.3));
+    
   }
-  
-  // define other methods down here.
 }
